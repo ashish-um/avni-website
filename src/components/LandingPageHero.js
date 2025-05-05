478fb01e0f9b2d867158509c9b092f24b41618d6
Ashish Umrao <ashishumrao632005@gmail.com>
2025-05-06 02:28:53 +0530

    pushed the button to atom directory

--- src/components/LandingPageHero.js
+++ src/components/LandingPageHero.js
@@ -8,7 +8,7 @@ import SecondaryCTAButton from "./SecondaryCTAButton";
 import Img from "gatsby-image";
 import {graphql, useStaticQuery} from 'gatsby';
 import StyledButton from './atom/StyledButton'
-import Button from './Button'
+import Button from './atom/Button'
 
 let subtitle = function (text) {
   return <p
@@ -83,9 +83,7 @@ export default function LandingPageHero() {
           {subtitle("Designed to work for all sectors")}
           {subtitle("Health, Education, Sanitation, Water, Waste, Agriculture, Social Security.")}
           <div style={{paddingTop: '1rem', flexDirection: 'column', display: 'flex', alignItems: 'center'}}>
-            {/* <SecondaryCTAButton text="Try for free" link={`/signup?${Constants.ContactSource}=${Constants.Trial}`}/> */}
-            <Button variant={'primary'}>Hello</Button>
-            <StyledButton>Try For Free</StyledButton>
+            <Button size={'large'} variant={'primary'}>Hello</Button>
             <p className="button is-primary is-medium" style={{marginTop: 10}}>
               <PopupText
                 text="Schedule a Demo"
